104106fa07317c22aaeef65ca118a70ae45e94fc
Rodion Smolyar <s1rodion123@gmail.com>
2025-10-14 22:59:13 +0300

    small changes

--- main.py
+++ main.py
@@ -21,10 +21,8 @@ def run_pipeline(image_path: list[str], source: bool) -> list:
         image = ImageLoader(path=path, source=source).load_image()
 
         if image is None:
-            res[i] = f"Не удалось загрузить изображение: {path}."
+            res[i].append(f"Не удалось загрузить изображение: {path}.")
             continue
-
-        res[i] = dict()
 
         # 1. Обработка изображения
 
@@ -33,10 +31,10 @@ def run_pipeline(image_path: list[str], source: bool) -> list:
         # 2. Нахождение признаков
 
         # нахождение предметов на фотографии
-        res[i]['детекция объектов'] = object_extractor.extract_features(image)
+        res[i].append(object_extractor.extract_features(image))
 
         # определение сцены
-        res[i]['сцена'] = scene_classificator.predict_scene(image)
+        res[i].append(scene_classificator.predict_scene(image))
 
     return res
 
@@ -44,4 +42,6 @@ def run_pipeline(image_path: list[str], source: bool) -> list:
 if __name__ == "__main__":
     # тесты
     # print(run_pipeline(['../../parking.png'], source=True))
-    print(run_pipeline(['https://carsharing-acceptances.s3.yandex.net/000080b5-5f40-4f6b-56bd-68e42cfe0f1d/car_location_22075860-8af6-11f0-b981-052fecb11955.CAP84636423206376342.jpg/46f3928-fad162a0-af3ae0d5-516d116e'], source=False))
+    print(run_pipeline(
+        ['https://carsharing-acceptances.s3.yandex.net/000080b5-5f40-4f6b-56bd-68e42cfe0f1d/car_location_22075860-8af6-11f0-b981-052fecb11955.CAP84636423206376342.jpg/46f3928-fad162a0-af3ae0d5-516d116e'],
+        source=False))
